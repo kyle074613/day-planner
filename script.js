e54a01afac84dbaaee5413d3c71c07c49964786b
kyle074613 <kylemj@uci.edu>
2019-12-21 20:02:44 -0800

    JS done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
 
 var currentTime = moment().format('HH');
-console.log(currentTime);
 
-var storedData = ["", "", "", "", "", "", "", "", ""];
+var userData = ["", "", "", "", "", "", "", "", ""];
 
-renderStoredData();
+var storedUserData = localStorage.getItem("userCalendar");
+if (storedUserData != null) {
+    userData = storedUserData.split(",");
+}
+
+
 
 for (var i = 0; i < 9; i++) {
     var rowTime = i + 9;
@@ -31,52 +35,21 @@ for (var i = 0; i < 9; i++) {
     //Generates a row for each work hour
     var newRow = $("<div class = 'row'>" +
         "<div class='col-2 py-2 hour'>" + formattedTime + "</div>" +
-        "<div class='col-8 p-0 description " + currentClass + "'><textarea class='w-100 h-100 user-input'></textarea></div>" +
+        "<div class='col-8 p-0 description " + currentClass + "'><textarea class='w-100 h-100 user-input'>" + userData[i] + "</textarea></div>" +
         "<div class='col-2 p-0 save-btn'><button class='saveBtn w-100 h-100'><i class='fas fa-save fa-2x'></i></button></div>" +
         "</div>");
-
-    // var newRow = $("<div class='row'>");
-    // var col1 = $("<div class = 'col-2 hour>'");
-    // col1.text(formattedTime);
-    // var col2 = $("<div class = 'col-8 description>");
-    // col2.attr('class', currentClass);
-    // var col3 = $("<div class='col-2 save-btn'>")
-    // newRow.append(col1, col2, col3);
-
+    //Adds a unique id for each row, used to store data
     newRow.attr('id', i);
 
     //Appends the new row to the page
     $(".container").append(newRow);
-
-
 }
 
-//
-function renderStoredData() {
-    if (storedData === ["", "", "", "", "", "", "", "", ""]) {
-        localStorage.setItem("userCalendar", storedData);
-    }
-    else
-        storedData = localStorage.getItem("userCalendar");
-}
-//
 
 $(".save-btn").on("click", function () {
     var currentRow = parseInt($(this).parent().attr('id'));
-
-    console.log(currentRow);
-
     var textInputVal = $(this).parent().children(".description").children(".user-input").val();
-    console.log(textInputVal);
 
-    //storedData[currentRow] = $(this).parent().children(".description").children(".user-input").val();
-
-
-    if (textInputVal === "")
-        storedData[currentRow] = textInputVal;
-    else
-        localStorage.setItem("userCalendar", storedData[currentRow]);
-
-
-    console.log(storedData);
+    userData[currentRow] = textInputVal;
+    localStorage.setItem("userCalendar", userData);
 });
